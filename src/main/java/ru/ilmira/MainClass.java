ec0616b695502916dd65b014c71654a5e7549e32
Ilmira <z.elle82@mail.ru>
2021-09-14 11:24:14 +0300

    Многопоточность. Гонка

--- src/main/java/ru/ilmira/MainClass.java
+++ src/main/java/ru/ilmira/MainClass.java
@@ -1,18 +1,31 @@
 package ru.ilmira;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        CountDownLatch prep = new CountDownLatch(CARS_COUNT);
+        CyclicBarrier start = new CyclicBarrier(CARS_COUNT);
+        CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT / 2), new Road(40));
+
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), prep, start, finish);
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+        for (Car car : cars) {
+            new Thread(car).start();
         }
+        prep.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        finish.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/ru/ilmira/Stage.java
+++ src/main/java/ru/ilmira/Stage.java
@@ -3,9 +3,7 @@ package ru.ilmira;
 public abstract class Stage {
     protected int length;
     protected String description;
-    public String getDescription() {
-        return description;
-    }
+
     public abstract void go(Car c);
 }
 
